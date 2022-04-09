05747d091574c6a0086eb1731245581b18245eca
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-04-09 21:39:53 +0530

    contracts deployed

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,21 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("dotenv").config({ path: ".env" });
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+  },
 };
+
+// Dogecoin deployed to: 0xDD83e5FB60c96342ECAEf41a56e89eB48ac00A2c
+// LinkToken deployed to: 0xB672a0e619888416481fe464b9C5AbC8747c39CF
+// DaiToken deployed to: 0x414153580a1C9e4fbF2D1809aB6cd6703905Ac0B
+// UsdcToken deployed to: 0x5605296d072180d8A6D2f94AD9a165339FEe2d1a
